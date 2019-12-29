3635f8693aedc8c75f4fd2f41bb94dcb6c9a2d8c
“kim” <kimbfsd@gmail.com>
2019-12-29 05:19:40 +0200

    Updates

---
+++ precache-manifest.29351138d69762d6406b69b756c1f749.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426e11b9c91342564c4a8a0776599424",
+    "url": "/photographer-spa/index.html"
+  },
+  {
+    "revision": "e472ea7b17e0fb84093c",
+    "url": "/photographer-spa/static/css/main.7e4c4ccc.chunk.css"
+  },
+  {
+    "revision": "0369cd3f4f8cf5907b6d",
+    "url": "/photographer-spa/static/js/2.fe824dc7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/photographer-spa/static/js/2.fe824dc7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e472ea7b17e0fb84093c",
+    "url": "/photographer-spa/static/js/main.68526df0.chunk.js"
+  },
+  {
+    "revision": "b0b90001ae2a0edc514b",
+    "url": "/photographer-spa/static/js/runtime-main.75e526dc.js"
+  }
+]);
